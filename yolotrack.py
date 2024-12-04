7a2aa0057b5f419b15a041e482898f2806ae5a9c
Yessin0 <yessin.toumi5680@istic.ucar.tn>
2024-12-04 16:56:39 +0100

    speed estimator using yolo10

---
+++ yolotrack.py
@@ -0,0 +1,58 @@
+import os
+import cv2
+from ultralytics import YOLO
+from speed import SpeedEstimator
+
+# Load YOLOv8 model
+model = YOLO("yolov10n.pt")
+# Initialize global variable to store cursor coordinates
+line_pts = [(0, 288), (1019, 288)]
+names = model.model.names  # This is a dictionary
+
+speed_obj = SpeedEstimator(reg_pts=line_pts, names=names)
+
+
+# Mouse callback function to capture mouse movement
+def rgb(event, x, y, flags, param):
+    global cursor_point
+    if event == cv2.EVENT_MOUSEMOVE:
+        cursor_point = (x, y)
+        print(f"Mouse coordinates: {cursor_point}")
+
+
+# Set up the window and attach the mouse callback function
+cv2.namedWindow('RGB')
+cv2.setMouseCallback('RGB', rgb)
+
+# Open the video file or webcam feed
+video_path = os.path.abspath('speed.mp4')
+print(f"Using video file at {video_path}")
+cap = cv2.VideoCapture('speed.mp4')
+
+count = 0
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Video stream ended or cannot be read.")
+        break
+
+    count += 1
+    if count % 3 != 0:  # Skip some frames for speed (optional)
+        continue
+
+    frame = cv2.resize(frame, (1020, 500))
+
+    # Perform object tracking
+    tracks = model.track(frame, persist=True, classes=[2, 7])
+
+    im0 = speed_obj.estimate_speed(frame, tracks)
+
+    # Display the frame with YOLOv8 results
+    cv2.imshow("RGB", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
